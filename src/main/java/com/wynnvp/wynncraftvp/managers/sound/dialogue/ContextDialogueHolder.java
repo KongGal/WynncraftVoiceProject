4680ee8397f94bc6b5c7dd7dc10f45715b62233a
Daniel <djjbusi@gmail.com>
2025-02-06 11:20:39 +0100

    Delete src/main directory

--- src/main/java/com/wynnvp/wynncraftvp/managers/sound/dialogue/ContextDialogueHolder.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright © Team-VoW 2024.
- * This file is released under AGPLv3. See LICENSE for full license details.
- */
-package com.wynnvp.wynncraftvp.managers.sound.dialogue;
-
-import java.util.List;
-
-public record ContextDialogueHolder(List<DialogueData> dialogues) {}
